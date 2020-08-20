d187b115b6ccb340544b621e128265824fd89135
dg0397 <rgg0397@gmail.com>
2020-08-20 19:48:04 -0400

    Adding more styles, main styles and main layout

--- src/components/SingleDetail/SingleDetail.jsx
+++ src/components/SingleDetail/SingleDetail.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import './SingleDetail.css'
-import Article from 'components/Article/Article';
 import { Link } from 'wouter';
 
 export default function Detail({id,article}){
+    const {title,extract,thumbnailSource} = article;
     const keyword = localStorage.getItem("keyword");
     return(
         <div className = "SingleDetail" >
-            <Article {...article}/>
-            <a href={`https://en.wikipedia.org/?curid=${id}`} target = "_blank" rel="noopener noreferrer" >Go to Original Article</a>
-            <Link to = {`/search/${keyword}`} >Go back to the List</Link>
+            <h1>{title}</h1>
+            <p>{extract}</p>
+            {   thumbnailSource && 
+                <div className = "container_img">
+                    <img src = {thumbnailSource} alt = {title} /> 
+                </div>
+            }
+            <a href={`https://en.wikipedia.org/?curid=${id}`} target = "_blank" rel="noopener noreferrer" id = 'original'>Go to Original Article</a>
+            <Link to = {`/search/${keyword}`} id = 'go_back'>Go back to the List</Link>
         </div>
     )
 }
