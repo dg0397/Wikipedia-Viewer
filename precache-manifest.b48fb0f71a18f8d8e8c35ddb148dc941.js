b2e1a56901f1163964b01bc9e4c6170d08a1439c
dg0397 <rgg0397@gmail.com>
2020-08-25 23:33:03 -0400

    Updates

---
+++ precache-manifest.b48fb0f71a18f8d8e8c35ddb148dc941.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7415a3eded91a9700e5cfb1f21e736",
+    "url": "/Wikipedia-Viewer/index.html"
+  },
+  {
+    "revision": "87b80d0de216196413e0",
+    "url": "/Wikipedia-Viewer/static/css/3.7669fde5.chunk.css"
+  },
+  {
+    "revision": "3c03ce48f73bfe100f16",
+    "url": "/Wikipedia-Viewer/static/css/4.42c2a667.chunk.css"
+  },
+  {
+    "revision": "71c258e37d673955309a",
+    "url": "/Wikipedia-Viewer/static/css/main.fc28cfbd.chunk.css"
+  },
+  {
+    "revision": "9fd92f1eff415ba0b3dc",
+    "url": "/Wikipedia-Viewer/static/js/2.28f4011a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Wikipedia-Viewer/static/js/2.28f4011a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87b80d0de216196413e0",
+    "url": "/Wikipedia-Viewer/static/js/3.9f7c95a6.chunk.js"
+  },
+  {
+    "revision": "3c03ce48f73bfe100f16",
+    "url": "/Wikipedia-Viewer/static/js/4.f71d8e87.chunk.js"
+  },
+  {
+    "revision": "79157a7027c5c23aa5fd",
+    "url": "/Wikipedia-Viewer/static/js/5.8ba0a80c.chunk.js"
+  },
+  {
+    "revision": "71c258e37d673955309a",
+    "url": "/Wikipedia-Viewer/static/js/main.8a2d8b8b.chunk.js"
+  },
+  {
+    "revision": "ba97cfc8ebfbd6d87c69",
+    "url": "/Wikipedia-Viewer/static/js/runtime-main.04d6fee2.js"
+  }
+]);
